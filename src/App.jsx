7e075a835c7fb506604435b434bd2fa33b92ff13
“Carina <ksfraan@hotmail.com>
2023-12-14 11:49:52 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
-import "./App.css";
-import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import './App.css';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import ExponentTwo from './components/ExponentTwo';
+import ExponentThree from './components/ExponentThree';
+import ExponentFour from './components/ExponentFour';
+import ExponentFive from './components/ExponentFive';
+import ExponentSix from './components/ExponentSix';
 
+function App() {
+    const [count, setCount] = useState(0);
 
-function App () {
-  return (
-    <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+    const increment = () => {
+        setCount(count + 1);
+    };
 
-      <br />
-      <h2><em>Exponents</em></h2>
+    const decrement = () => {
+        setCount(count - 1);
+    };
 
-      <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
-      </div>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h2>
+                <em>Counter</em>
+            </h2>
+
+            <Counter
+                count={count}
+                increment={increment}
+                decrement={decrement}
+            />
+
+            <br />
+            <h2>
+                <em>Exponents</em>
+            </h2>
+
+            <div className='container'>
+                <ExponentTwo count={count} />
+                <ExponentThree count={count} />
+                <ExponentFour count={count} />
+                <ExponentFive count={count} />
+                <ExponentSix count={count} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
